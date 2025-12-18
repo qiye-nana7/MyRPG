858a934dcf3bed0317b7aa696d6279eaaa27b331
Fraudster7 <2431695176@qq.com>
2025-12-19 00:24:36 +0800

    Chapter 1 part 7 "Enhanced Input"

---
+++ Source/MyRPG/3C/Controller/Player/AuraPlayerController.h
@@ -0,0 +1,17 @@
+// Learning Aura Project Only
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYRPG_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
